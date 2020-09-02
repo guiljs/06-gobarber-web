f326ba2ceeb1d789812a4d51866dca3655a59a52
Guilherme de Jesus Santos <guiljs@gmail.com>
2020-09-01 23:37:50 -0300

    Routes configuration and authentication

---
+++ src/pages/ForgotPassword/index.tsx
@@ -0,0 +1,79 @@
+import React, { useRef, useCallback } from 'react';
+import { FiLogIn, FiMail } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+
+import { Link } from 'react-router-dom';
+import { useToast } from '../../hooks/toast';
+
+import getValidationErrors from '../../utils/getValidationErrors';
+import logoImg from '../../assets/logo.svg';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+interface ForgotPasswordFormData {
+  email: string;
+  password: string;
+}
+
+const ForgotPassword: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const { addToast } = useToast();
+  const handleSubmit = useCallback(
+    async (data: ForgotPasswordFormData) => {
+      try {
+        formRef.current?.setErrors({});
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('Digite ume e-mail válido'),
+        });
+
+        await schema.validate(data, { abortEarly: false });
+
+        // Recuperaçãod de senha
+
+        // history.push('/dashboard');
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
+        }
+        addToast({
+          type: 'error',
+          title: 'Erro na recuperação de senha',
+          description:
+            'Ocorreu um erro ao tentar realizar a recuperação da senha, tente novamente.',
+        });
+      }
+    },
+    [addToast],
+  );
+
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Recuperar senha</h1>
+
+          <Input name="email" icon={FiMail} type="text" placeholder="E-mail" />
+
+          <Button type="submit">Recuperar</Button>
+        </Form>
+
+        <Link to="/signin">
+          <FiLogIn />
+          Voltar para o login
+        </Link>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
+
+export default ForgotPassword;
